ea895ee14d3698a0f093d73199e1a033ac6b6458
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-03-31 22:54:01 +0200

    feat: improved particle shape by implementing light point, an improved version of Diffuse point that makes the circle "fade" the more we go outwards, from a value of 1 (at the center of the circle) until it disappears at the edge where the value is 0

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -2,8 +2,18 @@ void main() {
     #include <colorspace_fragment>
 
     // Disc
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength = 1.0 - step(0.5, strength);
+
+    // Diffuse point
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength *= 2.0;
+    // strength = 1.0 - strength;
+
+    // Light point
     float strength = distance(gl_PointCoord, vec2(0.5));
-    strength = 1.0 - step(0.5, strength);
+    strength = 1.0 - strength;
+    strength = pow(strength, 10.0);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
