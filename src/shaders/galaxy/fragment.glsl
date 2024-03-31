20bba2b42ce3579c6f75c4f4d8dd587a6fce2599
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-03-31 22:47:35 +0200

    feat: particles are now rendered as circles

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,5 +1,9 @@
 void main() {
     #include <colorspace_fragment>
 
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    // Disc
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = 1.0 - step(0.5, strength);
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
